a123329f6afb8fd691a0c7c2fa353bd0dc6eac2b
zhoujian9410 <zhoujian9410@qq.com>
2017-07-08 12:20:59 +0800

    3D kmeans

--- test/kmeans_3D.py
+++ test/kmeans_3D.py
@@ -2,9 +2,9 @@ import numpy as np
 
 def get_J(data,center):
     data_len=data.shape[1]
-    tmp=np.zeros((2,center.shape[1],data_len),dtype=np.float64)
+    tmp=np.zeros((3,center.shape[1],data_len),dtype=np.float64)
     center_len=center.shape[1]
-    for i in range(2):
+    for i in range(3):
         for j in range(center_len):
             for k in range(data_len):
                 tmp[i][j][k]=data[i][k]-center[i][j]
@@ -19,9 +19,9 @@ def get_J(data,center):
     return J,minpos
 
 def cal_center(data,num_k,minpos):
-    newc=np.zeros((2,num_k))
+    newc=np.zeros((3,num_k))
     newc=np.array(newc,dtype=np.float64)
-    for i in range(2):
+    for i in range(3):
         for j in range(num_k):
             num_in_=0
             for k in range(data.shape[1]):
@@ -35,17 +35,15 @@ def cal_center(data,num_k,minpos):
     return newc
     
 
-def keans_2(data,k):##the data is 2D,(x,y)
-    data=np.array(data,dtype=np.float64)
-    data=data.reshape((2,-1))
+def keans_3(data,k):##the data is 2D,(x,y)
     data_len=data.shape[1]
     #get k center for each demensions 
     chose=np.random.random(k)
     chose = chose*data_len
     chose=np.floor(chose)
     chose=np.array(chose,dtype=np.int64)
-    center=np.zeros((2,k),dtype=np.float64)
-    for i in range(2):
+    center=np.zeros((3,k),dtype=np.float64)
+    for i in range(3):
         center[i]=np.take(data[i],chose)
     J,minpos=get_J(data,center)
     #Jold=J
@@ -63,8 +61,8 @@ def keans_2(data,k):##the data is 2D,(x,y)
     return new_center,minpos
 
 def new_image(data,k):
-    center,minpos=keans_2(data,k)
-    for i in range(2):
+    center,minpos=keans_3(data,k)
+    for i in range(3):
         for j in range(k):
             for ij in range(data.shape[1]):
                 if minpos[ij]==j:
@@ -73,11 +71,17 @@ def new_image(data,k):
                     data[i][ij]=data[i][ij]
     return data 
     
-    
-    
+def change_shape(data,k):
+    data=np.array(data,dtype=np.float64)
+    size=data.shape
+    lenth=size[0]*size[1]
+    data=data.reshape((lenth,3))
+    new_data1=new_image(data.T,k)
+    new_data=new_data.T
+    new_data=new_data.reshape((size[0],size[1],size[2]),dtype=np.uint64)
+    return new_data
     
     
     
         
-
 	
